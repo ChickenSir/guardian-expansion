3fabb75065b01ef33ec8d8782b536e2e5745bf3f
ChickenSir <shyguyman11@gmail.com>
2024-07-12 19:35:40 +1000

    Add Tools and Armor to Item Collection

--- src/main/java/com/example/guardianexpansion/Collections/ItemsCollection.java
+++ src/main/java/com/example/guardianexpansion/Collections/ItemsCollection.java
@@ -1,14 +1,23 @@
 package com.example.guardianexpansion.Collections;
 
+import com.example.guardianexpansion.Materials.PrismarineArmorMaterial;
 import com.example.guardianexpansion.Materials.PrismarineToolMaterial;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 import net.minecraft.item.ToolItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorItem.Type;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
 
 public class ItemsCollection {
     // Mod Tools
@@ -17,4 +26,26 @@ public class ItemsCollection {
     public static final ToolItem PRISMARINE_AXE = new AxeItem(PrismarineToolMaterial.INSTANCE, 0, 0f, new FabricItemSettings());
     public static final ToolItem PRISMARINE_SHOVEL = new ShovelItem(PrismarineToolMaterial.INSTANCE, 0, 0f, new FabricItemSettings());
     public static final ToolItem PRISMARINE_HOE = new HoeItem(PrismarineToolMaterial.INSTANCE, 0, 0f, new FabricItemSettings());
+
+    // Mod Armor
+    public static final ArmorMaterial PRISMARINE_ARMOR_MATERIAL = new PrismarineArmorMaterial();
+    public static final Item PRISMARINE_HELMET = new ArmorItem(PRISMARINE_ARMOR_MATERIAL, Type.HELMET, new Item.Settings());
+    public static final Item PRISMARINE_CHESTPLATE = new ArmorItem(PRISMARINE_ARMOR_MATERIAL, Type.CHESTPLATE, new Item.Settings());
+    public static final Item PRISMARINE_LEGGINGS = new ArmorItem(PRISMARINE_ARMOR_MATERIAL, Type.LEGGINGS, new Item.Settings());
+    public static final Item PRISMARINE_BOOTS = new ArmorItem(PRISMARINE_ARMOR_MATERIAL, Type.BOOTS, new Item.Settings());
+
+    public static void Register(String modID) {
+        // Register Tools
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_sword"), PRISMARINE_SWORD);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_pickaxe"), PRISMARINE_PICKAXE);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_axe"), PRISMARINE_AXE);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_shovel"), PRISMARINE_SHOVEL);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_hoe"), PRISMARINE_HOE);
+
+        // Register Armor
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_helmet"), PRISMARINE_HELMET);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_chestplate"), PRISMARINE_CHESTPLATE);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_leggings"), PRISMARINE_LEGGINGS);
+        Registry.register(Registries.ITEM, new Identifier(modID, "prismarine_boots"), PRISMARINE_BOOTS);
+    }
 }
---
+++ src/main/java/com/example/guardianexpansion/Materials/PrismarineArmorMaterial.java
@@ -0,0 +1,62 @@
+package com.example.guardianexpansion.Materials;
+
+import net.minecraft.item.ArmorItem.Type;
+
+import com.example.guardianexpansion.GuardianExpansion;
+
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class PrismarineArmorMaterial implements ArmorMaterial {
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {2, 5, 7, 2};
+    private static final int durabilityMultipleer = 25;
+    private static final int enchantability = 12;
+    private static final String name = "prismarine";
+    private static final float toughness = 1f;
+    private static float knockbackResistance = 2f;
+
+    @Override
+    public int getDurability(Type type) {
+        return BASE_DURABILITY[type.ordinal()] * durabilityMultipleer;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_TURTLE;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance;
+    }
+
+    @Override
+    public String getName() {
+        return GuardianExpansion.modID + ":" + name;
+    }
+
+    @Override
+    public int getProtection(Type type) {
+        return PROTECTION_VALUES[type.ordinal()];
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.PRISMARINE);
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+    
+}
