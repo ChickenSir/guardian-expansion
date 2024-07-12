e9112eb1a1c3a9db147365ca8dc36cfadb6e9fe7
ChickenSir <shyguyman11@gmail.com>
2024-07-12 19:42:40 +1000

    Add item group and registry to GuardianExpansion.java

--- src/main/java/com/example/guardianexpansion/GuardianExpansion.java
+++ src/main/java/com/example/guardianexpansion/GuardianExpansion.java
@@ -1,9 +1,19 @@
 package com.example.guardianexpansion;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.example.guardianexpansion.Collections.ItemsCollection;
 
 public class GuardianExpansion implements ModInitializer {
 
@@ -13,9 +23,32 @@ public class GuardianExpansion implements ModInitializer {
 	public static final String modName = "Guardian Expansion";
 
 	// Item Group For Modded Content
+	private static final ItemGroup GUARDIAN_EXPANSION_GROUP = FabricItemGroup.builder()
+		.icon(() -> new ItemStack(Items.PRISMARINE))
+		.displayName(Text.translatable(modName))
+		.entries((context, entries) -> {
+			entries.add(ItemsCollection.PRISMARINE_SWORD);
+			entries.add(ItemsCollection.PRISMARINE_PICKAXE);
+			entries.add(ItemsCollection.PRISMARINE_AXE);
+			entries.add(ItemsCollection.PRISMARINE_SHOVEL);
+			entries.add(ItemsCollection.PRISMARINE_HOE);
+			entries.add(ItemsCollection.PRISMARINE_HELMET);
+			entries.add(ItemsCollection.PRISMARINE_CHESTPLATE);
+			entries.add(ItemsCollection.PRISMARINE_LEGGINGS);
+			entries.add(ItemsCollection.PRISMARINE_BOOTS);
+		})
+		.build();
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		// Register Items
+		LOGGER.info("Registering Items!");
+
+		ItemsCollection.Register(modID);
+
+		// Register Item Group
+		LOGGER.info("Registerting Item Group!");
+
+		Registry.register(Registries.ITEM_GROUP, new Identifier(modID, "guardian_expansion_group"), GUARDIAN_EXPANSION_GROUP);
 	}
 }
