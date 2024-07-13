2782ed1a825779557febb81cffb71eb26120f554
ChickenSir <shyguyman11@gmail.com>
2024-07-13 16:34:12 +1000

    Update properties of guardian armor

--- src/main/java/com/example/guardianexpansion/Materials/GuardianArmorMaterial.java
+++ src/main/java/com/example/guardianexpansion/Materials/GuardianArmorMaterial.java
@@ -10,13 +10,13 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
 public class GuardianArmorMaterial implements ArmorMaterial {
-    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
-    private static final int[] PROTECTION_VALUES = new int[] {2, 7, 5, 2};
-    private static final int durabilityMultipleer = 25;
-    private static final int enchantability = 12;
-    private static final String name = "prismarine";
+    private static final int[] BASE_DURABILITY = new int[] {16, 25, 20, 18};
+    private static final int[] PROTECTION_VALUES = new int[] {3, 7, 5, 3};
+    private static final int durabilityMultipleer = 20;
+    private static final int enchantability = 15;
+    private static final String name = "guardian";
     private static final float toughness = 1f;
-    private static float knockbackResistance = 2f;
+    private static float knockbackResistance = 1f;
 
     @Override
     public int getDurability(Type type) {
