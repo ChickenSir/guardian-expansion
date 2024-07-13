a4441eccd2d71d16ebec5eabfe13183b1b86265d
ChickenSir <shyguyman11@gmail.com>
2024-07-13 16:19:07 +1000

    Add Guardian ArmorMaterial and edit protection values for prismarine armor

--- src/main/java/com/example/guardianexpansion/Materials/GuardianArmorMaterial.java
+++ src/main/java/com/example/guardianexpansion/Materials/GuardianArmorMaterial.java
@@ -1,16 +1,15 @@
 package com.example.guardianexpansion.Materials;
 
-import net.minecraft.item.ArmorItem.Type;
-
 import com.example.guardianexpansion.GuardianExpansion;
 
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ArmorItem.Type;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
-public class PrismarineArmorMaterial implements ArmorMaterial {
+public class GuardianArmorMaterial implements ArmorMaterial {
     private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
     private static final int[] PROTECTION_VALUES = new int[] {2, 7, 5, 2};
     private static final int durabilityMultipleer = 25;
@@ -58,5 +57,4 @@ public class PrismarineArmorMaterial implements ArmorMaterial {
     public float getToughness() {
         return toughness;
     }
-    
 }
