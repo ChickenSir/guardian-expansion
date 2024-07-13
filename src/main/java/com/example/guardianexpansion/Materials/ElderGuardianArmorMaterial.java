a25e1e36fd33809abc47732aeb620800ef256f62
ChickenSir <shyguyman11@gmail.com>
2024-07-13 16:36:49 +1000

    Add elder guardian armor material

---
+++ src/main/java/com/example/guardianexpansion/Materials/ElderGuardianArmorMaterial.java
@@ -0,0 +1,60 @@
+package com.example.guardianexpansion.Materials;
+
+import com.example.guardianexpansion.GuardianExpansion;
+
+import net.minecraft.item.ArmorItem.Type;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class ElderGuardianArmorMaterial implements ArmorMaterial {
+    private static final int[] BASE_DURABILITY = new int[] {16, 25, 20, 18};
+    private static final int[] PROTECTION_VALUES = new int[] {3, 7, 6, 3};
+    private static final int durabilityMultipleer = 24;
+    private static final int enchantability = 18;
+    private static final String name = "elder_guardian";
+    private static final float toughness = 2.5f;
+    private static float knockbackResistance = 1f;
+
+    @Override
+    public int getDurability(Type type) {
+        return BASE_DURABILITY[type.ordinal()] * durabilityMultipleer;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_TURTLE;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance;
+    }
+
+    @Override
+    public String getName() {
+        return GuardianExpansion.modID + ":" + name;
+    }
+
+    @Override
+    public int getProtection(Type type) {
+        return PROTECTION_VALUES[type.ordinal()];
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.PRISMARINE);
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+}
