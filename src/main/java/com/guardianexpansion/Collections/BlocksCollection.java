7ee2b05ac5fed3372f56402207a45a25a13c6a2a
ChickenSir <shyguyman11@gmail.com>
2024-07-14 14:00:59 +1000

    Add datagen

--- src/main/java/com/guardianexpansion/Collections/BlocksCollection.java
+++ src/main/java/com/guardianexpansion/Collections/BlocksCollection.java
@@ -4,6 +4,9 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.WallBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,6 +18,18 @@ public class BlocksCollection {
     public static final Block ELDER_PRISMARINE_BRICKS = new Block(FabricBlockSettings.copyOf(Blocks.PRISMARINE_BRICKS).requiresTool());
     public static final Block ELDER_DARK_PRISMARINE = new Block(FabricBlockSettings.copyOf(Blocks.DARK_PRISMARINE).requiresTool());
 
+    public static final Block ELDER_PRISMARINE_STAIRS = new StairsBlock(ELDER_PRISMARINE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_PRISMARINE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_PRISMARINE_WALL = new WallBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+
+    public static final Block ELDER_PRISMARINE_BRICKS_STAIRS = new StairsBlock(ELDER_PRISMARINE_BRICKS.getDefaultState(), FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_PRISMARINE_BRICKS_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_PRISMARINE_BRICKS_WALL = new WallBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+
+    public static final Block ELDER_DARK_PRISMARINE_STAIRS = new StairsBlock(ELDER_DARK_PRISMARINE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_DARK_PRISMARINE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+    public static final Block ELDER_DARK_PRISMARINE_WALL = new WallBlock(FabricBlockSettings.copyOf(Blocks.PRISMARINE).requiresTool());
+
     public static void Register(String modID) {
         // Register Blocks
         Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine"), ELDER_PRISMARINE);
@@ -25,5 +40,26 @@ public class BlocksCollection {
         
         Registry.register(Registries.BLOCK, new Identifier(modID, "elder_dark_prismarine"), ELDER_DARK_PRISMARINE);
         Registry.register(Registries.ITEM, new Identifier(modID, "elder_dark_prismarine"), new BlockItem(ELDER_DARK_PRISMARINE, new FabricItemSettings()));
+
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_stairs"), ELDER_PRISMARINE_STAIRS);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_stairs"), new BlockItem(ELDER_PRISMARINE_STAIRS, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_slab"), ELDER_PRISMARINE_SLAB);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_slab"), new BlockItem(ELDER_PRISMARINE_SLAB, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_wall"), ELDER_PRISMARINE_WALL);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_wall"), new BlockItem(ELDER_PRISMARINE_WALL, new FabricItemSettings()));
+
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_bricks_stairs"), ELDER_PRISMARINE_BRICKS_STAIRS);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_bricks_stairs"), new BlockItem(ELDER_PRISMARINE_BRICKS_STAIRS, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_bricks_slab"), ELDER_PRISMARINE_BRICKS_SLAB);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_bricks_slab"), new BlockItem(ELDER_PRISMARINE_BRICKS_SLAB, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_prismarine_bricks_wall"), ELDER_PRISMARINE_BRICKS_WALL);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_prismarine_bricks_wall"), new BlockItem(ELDER_PRISMARINE_BRICKS_WALL, new FabricItemSettings()));
+
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_dark_prismarine_stairs"), ELDER_DARK_PRISMARINE_STAIRS);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_dark_prismarine_stairs"), new BlockItem(ELDER_DARK_PRISMARINE_STAIRS, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_dark_prismarine_slab"), ELDER_DARK_PRISMARINE_SLAB);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_dark_prismarine_slab"), new BlockItem(ELDER_DARK_PRISMARINE_SLAB, new FabricItemSettings()));
+        Registry.register(Registries.BLOCK, new Identifier(modID, "elder_dark_prismarine_wall"), ELDER_DARK_PRISMARINE_WALL);
+        Registry.register(Registries.ITEM, new Identifier(modID, "elder_dark_prismarine_wall"), new BlockItem(ELDER_DARK_PRISMARINE_WALL, new FabricItemSettings()));
     }
 }
--- src/main/java/com/guardianexpansion/datagen/GuardianExpansionDataGenerator.java
+++ src/main/java/com/guardianexpansion/datagen/GuardianExpansionDataGenerator.java
@@ -1,20 +1,36 @@
 package com.guardianexpansion.datagen;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import com.guardianexpansion.Collections.BlocksCollection;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider.BlockTagProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.Identifier;
 
 public class GuardianExpansionDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
+
+        pack.addProvider(GEBlockTagGenerator::new);
+        pack.addProvider(GELootTableProvider::new);
+        pack.addProvider(GEModelProvider::new);
+        pack.addProvider(GERecipeProvider::new);
     }
 
     private static class GEBlockTagGenerator extends BlockTagProvider {
@@ -25,7 +41,98 @@ public class GuardianExpansionDataGenerator implements DataGeneratorEntrypoint {
 
         @Override
         protected void configure(RegistryWrapper.WrapperLookup arg) {
-            getOrCreateTagBuilder(BlockTags.WALLS);
+            getOrCreateTagBuilder(BlockTags.WALLS)
+                .add(BlocksCollection.ELDER_PRISMARINE_WALL)
+                .add(BlocksCollection.ELDER_PRISMARINE_BRICKS_WALL)
+                .add(BlocksCollection.ELDER_DARK_PRISMARINE_WALL);
+
+            getOrCreateTagBuilder(BlockTags.SLABS)
+                .add(BlocksCollection.ELDER_PRISMARINE_SLAB)
+                .add(BlocksCollection.ELDER_PRISMARINE_BRICKS_SLAB)
+                .add(BlocksCollection.ELDER_DARK_PRISMARINE_SLAB);
+
+            getOrCreateTagBuilder(BlockTags.STAIRS)
+                .add(BlocksCollection.ELDER_PRISMARINE_STAIRS)
+                .add(BlocksCollection.ELDER_PRISMARINE_BRICKS_STAIRS)
+                .add(BlocksCollection.ELDER_DARK_PRISMARINE_STAIRS);
+        }
+        
+    }
+
+    private static class GELootTableProvider extends FabricBlockLootTableProvider {
+        protected GELootTableProvider(FabricDataOutput dataOutput) {
+            super(dataOutput);
+        }
+
+        @Override
+        public void generate() {
+            addDrop(BlocksCollection.ELDER_PRISMARINE_STAIRS);
+            addDrop(BlocksCollection.ELDER_PRISMARINE_SLAB, slabDrops(BlocksCollection.ELDER_PRISMARINE_SLAB));
+            addDrop(BlocksCollection.ELDER_PRISMARINE_WALL);
+
+            addDrop(BlocksCollection.ELDER_PRISMARINE_BRICKS_STAIRS);
+            addDrop(BlocksCollection.ELDER_PRISMARINE_BRICKS_SLAB, slabDrops(BlocksCollection.ELDER_PRISMARINE_BRICKS_SLAB));
+            addDrop(BlocksCollection.ELDER_PRISMARINE_BRICKS_WALL);
+
+            addDrop(BlocksCollection.ELDER_DARK_PRISMARINE_STAIRS);
+            addDrop(BlocksCollection.ELDER_DARK_PRISMARINE_SLAB, slabDrops(BlocksCollection.ELDER_DARK_PRISMARINE_SLAB));
+            addDrop(BlocksCollection.ELDER_DARK_PRISMARINE_WALL);
+        }
+        
+    }
+
+    private static class GEModelProvider extends FabricModelProvider {
+
+        public GEModelProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+            BlockStateModelGenerator.BlockTexturePool elder_prismarine_pool = blockStateModelGenerator.registerCubeAllModelTexturePool(BlocksCollection.ELDER_PRISMARINE);
+            BlockStateModelGenerator.BlockTexturePool elder_prismarine_bricks_pool = blockStateModelGenerator.registerCubeAllModelTexturePool(BlocksCollection.ELDER_PRISMARINE_BRICKS);
+            BlockStateModelGenerator.BlockTexturePool elder_dark_prismarine_pool = blockStateModelGenerator.registerCubeAllModelTexturePool(BlocksCollection.ELDER_DARK_PRISMARINE);
+
+            elder_prismarine_pool.stairs(BlocksCollection.ELDER_PRISMARINE_STAIRS);
+            elder_prismarine_pool.slab(BlocksCollection.ELDER_PRISMARINE_SLAB);
+            elder_prismarine_pool.wall(BlocksCollection.ELDER_PRISMARINE_WALL);
+
+            elder_prismarine_bricks_pool.stairs(BlocksCollection.ELDER_PRISMARINE_BRICKS_STAIRS);
+            elder_prismarine_bricks_pool.slab(BlocksCollection.ELDER_PRISMARINE_BRICKS_SLAB);
+            elder_prismarine_bricks_pool.wall(BlocksCollection.ELDER_PRISMARINE_BRICKS_WALL);
+
+            elder_dark_prismarine_pool.stairs(BlocksCollection.ELDER_DARK_PRISMARINE_STAIRS);
+            elder_dark_prismarine_pool.slab(BlocksCollection.ELDER_DARK_PRISMARINE_SLAB);
+            elder_dark_prismarine_pool.wall(BlocksCollection.ELDER_DARK_PRISMARINE_WALL);
+        }
+
+        @Override
+        public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+            
+        }
+        
+    }
+
+    private static class GERecipeProvider extends FabricRecipeProvider {
+
+        public GERecipeProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generate(Consumer<RecipeJsonProvider> exporter) {
+            createStairsRecipe(BlocksCollection.ELDER_PRISMARINE, Ingredient.ofItems(BlocksCollection.ELDER_PRISMARINE))
+                .criterion(hasItem(BlocksCollection.ELDER_PRISMARINE), conditionsFromItem(BlocksCollection.ELDER_PRISMARINE))
+                .offerTo(exporter, new Identifier(getRecipeName(BlocksCollection.ELDER_PRISMARINE_STAIRS)));
+
+            createStairsRecipe(BlocksCollection.ELDER_PRISMARINE_BRICKS, Ingredient.ofItems(BlocksCollection.ELDER_PRISMARINE_BRICKS))
+                .criterion(hasItem(BlocksCollection.ELDER_PRISMARINE_BRICKS), conditionsFromItem(BlocksCollection.ELDER_PRISMARINE_BRICKS))
+                .offerTo(exporter, new Identifier(getRecipeName(BlocksCollection.ELDER_PRISMARINE_BRICKS_STAIRS)));
+
+            createStairsRecipe(BlocksCollection.ELDER_DARK_PRISMARINE, Ingredient.ofItems(BlocksCollection.ELDER_DARK_PRISMARINE))
+                .criterion(hasItem(BlocksCollection.ELDER_DARK_PRISMARINE), conditionsFromItem(BlocksCollection.ELDER_DARK_PRISMARINE))
+                .offerTo(exporter, new Identifier(getRecipeName(BlocksCollection.ELDER_DARK_PRISMARINE_STAIRS)));
+
         }
         
     }
