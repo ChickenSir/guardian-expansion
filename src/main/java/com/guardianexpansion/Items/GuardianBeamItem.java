3c3b529e75e4e3563a392795aa793ea26c756af8
ChickenSir <shyguyman11@gmail.com>
2024-07-16 17:37:49 +1000

    Improve guardian beam recharge mechanic

--- src/main/java/com/guardianexpansion/Items/GuardianBeamItem.java
+++ src/main/java/com/guardianexpansion/Items/GuardianBeamItem.java
@@ -1,6 +1,5 @@
 package com.guardianexpansion.Items;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,7 +10,6 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class GuardianBeamItem extends Item {
-    private boolean isRecharging = false;
 
     public GuardianBeamItem(Settings settings) {
         super(settings);
@@ -22,27 +20,14 @@ public class GuardianBeamItem extends Item {
         if (!world.isClient()) {
             ItemStack stack = user.getStackInHand(hand);
             if (stack.getDamage() >= stack.getMaxDamage() - 1) {
-                isRecharging = true;
+                user.getItemCooldownManager().set(this, 300);
+                stack.setDamage(0);
             }
-            if (!isRecharging) {
-                stack.setDamage(stack.getDamage() + 1);
+            stack.setDamage(stack.getDamage() + 1);
 
-                world.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_GUARDIAN_ATTACK, SoundCategory.PLAYERS, 1f, 1f);
-            }
+            world.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_GUARDIAN_ATTACK, SoundCategory.PLAYERS, 1f, 1f);
         }
 
         return TypedActionResult.pass(user.getStackInHand(hand));
-    }
-
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (!world.isClient()) {
-            if (isRecharging) {
-                stack.setDamage(stack.getDamage() - 1);
-                if (stack.getDamage() == 0) {
-                    isRecharging = false;
-                }
-            }
-        }
     }
 }
